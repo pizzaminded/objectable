ff67bff69dd1b44b3a9ecbda845ed2e2a24b1e07
pizzaminded <pizzaminded@users.noreply.github.com>
2019-09-05 23:28:16 +0200

    added properties to annotation

--- Annotation/Header.php
+++ Annotation/Header.php
@@ -5,8 +5,28 @@ namespace Pizzaminded\Objectable\Annotation;
 /**
  * @Annotation
  * @author pizzaminded <miki@appvende.net>
+ * @license MIT
  */
 class Header
 {
+    /**
+     * @var string|null
+     */
+    protected $title;
+
+    /**
+     * @var int|null
+     */
+    protected $order;
+
+    /**
+     * Header constructor.
+     * @param array $values
+     */
+    public function __construct(array $values)
+    {
+        $this->title = $values['title'] ?? null;
+        $this->order = $values['order'] ?? null;
+    }
 
 }
