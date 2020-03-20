1d6d91d9d92ed6cf3542526301caa1f01e172de4
pizzaminded <pizzaminded@users.noreply.github.com>
2020-03-20 22:27:11 +0100

    modify interface definition

--- Transformer/ActionFieldTransformerInterface.php
+++ Transformer/ActionFieldTransformerInterface.php
@@ -12,7 +12,16 @@ interface ActionFieldTransformerInterface
 {
     /**
      * @param ActionField $actionField Annotation taken from given entity
+     * @param object $entity Currently processed entity
      * @return string
      */
-    public function transformActionUrl(ActionField $actionField): string;
+    public function transformActionUrl(ActionField $actionField, object $entity): string;
+
+    /**
+     * Allows to transform given label in user-defined way.
+     * @param ActionField $actionField Annotation taken from given entity
+     * @param object $entity Currently processed entity
+     * @return string
+     */
+    public function transformActionLabel(ActionField $actionField, object $entity): string;
 }
