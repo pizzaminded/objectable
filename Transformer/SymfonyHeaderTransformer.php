43b28c50f841478d0138ce9c99039a036001fb1f
pizzaminded <pizzaminded@users.noreply.github.com>
2020-11-21 23:03:29 +0100

    feat: add SymfonyHeaderTransformer

---
+++ Transformer/SymfonyHeaderTransformer.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Pizzaminded\Objectable\Transformer;
+
+
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * @author pizzaminded <mikolajczajkowsky@gmail.com>
+ * @license MIT
+ */
+class SymfonyHeaderTransformer implements HeaderTransformerInterface
+{
+    protected TranslatorInterface $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function transform(string $title, string $className, string $propertyName): string
+    {
+       return $this->translator->trans($title);
+    }
+}
