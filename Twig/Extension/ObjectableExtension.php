412aab73b35f874b4049c0379d6c0df9df01c8cd
pizzaminded <pizzaminded@users.noreply.github.com>
2021-02-18 00:59:09 +0100

    refactor: drop render single method as is does not exists so far, php8 improvements

--- Twig/Extension/ObjectableExtension.php
+++ Twig/Extension/ObjectableExtension.php
@@ -1,13 +1,15 @@
 <?php
+declare(strict_types=1);
 
 namespace Pizzaminded\Objectable\Twig\Extension;
 
 use Pizzaminded\Objectable\Objectable;
+use Pizzaminded\Objectable\ObjectableException;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 /**
- * @author pizzaminded <miki@appvende.net>
+ * @author pizzaminded <mikolajczajkowsky@gmail.com>
  * @license MIT
  */
 class ObjectableExtension extends AbstractExtension
@@ -16,12 +18,8 @@ class ObjectableExtension extends AbstractExtension
     /**
      * @var Objectable
      */
-    protected $objectable;
+    protected Objectable $objectable;
 
-    /**
-     * ObjectableExtension constructor.
-     * @param Objectable $objectable
-     */
     public function __construct(Objectable $objectable)
     {
         $this->objectable = $objectable;
@@ -30,31 +28,22 @@ class ObjectableExtension extends AbstractExtension
     /**
      * @return TwigFunction[]
      */
-    public function getFunctions()
+    public function getFunctions(): array
     {
         return [
             new TwigFunction('objectable', [$this, 'renderTable'], ['is_safe' => ['html']]),
-            new TwigFunction('objectable_single', [$this, 'renderSingle'], ['is_safe' => ['html']])
         ];
     }
 
     /**
-     * @param array $data
+     * @param array|\Iterator $data
      * @return string
-     * @throws \Pizzaminded\Objectable\ObjectableException
+     * @throws ObjectableException
      * @throws \ReflectionException
      */
-    public function renderTable($data): string
+    public function renderTable(array|\Iterator $data): string
     {
         return $this->objectable->renderTable($data);
     }
 
-    /**
-     * @param object $data
-     * @return string
-     */
-    public function renderSingle($data): string
-    {
-        return $this->objectable->renderSingleObject($data);
-    }
 }
